a30863a620fff5380fc2242460e3fb2d06587f1c
Jhojan Camilo Rodriguez Castañeda <jhojancamilorodriguez2017@gmail.com>
2026-06-01 10:26:54 -0500

    Connect Task router

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserLogin
 from app.models.user import User
 from app.db.dependencies import get_db
-from app.auth.hash import hash_password
+from app.auth.hash import hash_password, verify_password
+from app.auth.jwt_handler import create_access_token
 
 router = APIRouter()
 
@@ -24,4 +25,37 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
     return {
         "message": "User created successfully"
+    }
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    if not verify_password(
+        user.password,
+        db_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    access_token = create_access_token(
+        data={
+            "sub": db_user.email
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
     }
